a7aa574b52445499ae11eb3cc578089f71cd0c3c
Omar Ruiz <omar.ruiz0531@gmail.com>
2025-08-28 14:44:23 -0600

    First commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Di algo");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.next();
+
+        System.out.println("User wrote "+ userInput);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Ejercicio Repositorio de github 
+
+El porgrama esta hecho en java, este toma una entrada del usuario y la devuelve posteriormente en la consola
+
+'''
