4158c24438cb8070bb2353da5505d55a91f0f780
Omar Ruiz <omar.ruiz0531@gmail.com>
2025-08-28 15:29:39 -0600

    Nuevos cambios

--- Main.java
+++ Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Di algo");
-
+	for(int i=0; i<=10;i++){
+	System.out.println(i);		
+}
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.next();
 
