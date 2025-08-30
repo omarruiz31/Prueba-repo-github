334c918eadc4a6585622f11431c4fb3ffc3fd83e
Omar Ruiz <omar.ruiz0531@gmail.com>
2025-08-29 23:42:46 -0600

    Tercer commit

--- Main.java
+++ Main.java
@@ -4,11 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Di algo");
-	for(int i=0; i<=10;i++){
-	System.out.println(i);
-        System.out.println(i);		
-}
+        System.out.print("Di algo:");
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.next();
 
